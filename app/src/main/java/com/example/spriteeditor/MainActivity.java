d8bb97ad58f30cdf90d7dbacdde7cad47d7c487e
Quang Huy Le <beatboy2603@gmail.com>
2019-03-04 14:35:06 +0700

    abc

--- app/src/main/java/com/example/spriteeditor/MainActivity.java
+++ app/src/main/java/com/example/spriteeditor/MainActivity.java
@@ -1,21 +1,75 @@
 package com.example.spriteeditor;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
-PixelCanvas pixelCanvas;
+    PixelCanvas pixelCanvas;
+    Button btnImport;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pixelCanvas = findViewById(R.id.pc);
-        pixelCanvas.post(new Runnable() {
+//        pixelCanvas.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                pixelCanvas.setBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.piskel));
+//            }
+//        });
+
+
+
+        btnImport = findViewById(R.id.btnImport);
+        btnImport.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                pixelCanvas.setBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.piskel));
+            public void onClick(View v) {
+                openGallery();
             }
         });
+    }
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            try {
+                imageUri = data.getData();
+                InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                pixelCanvas.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        pixelCanvas.setBitmap(selectedImage);
+                    }
+                });
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
