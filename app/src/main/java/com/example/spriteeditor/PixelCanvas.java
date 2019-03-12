da8f8539365a64ae6faeaefee9aefc70fb783b5c
Think <phamduythinh258@gmail.com>
2019-03-12 09:34:52 +0700

    GraphicUlti

--- app/src/main/java/com/example/spriteeditor/PixelCanvas.java
+++ app/src/main/java/com/example/spriteeditor/PixelCanvas.java
@@ -19,7 +19,8 @@ public class PixelCanvas extends View {
         CUT,
         MOVE,
         FILL,
-        CIRCLE
+        CIRCLE,
+        PICK
     }
 
     Bitmap bitmap;
@@ -39,8 +40,6 @@ public class PixelCanvas extends View {
     private float scale = 1;
     private float minScale;
 
-    private int shapeStartX;
-    private int shapeStartY;
     private float pvTop;
     private float pvLeft;
 
@@ -60,7 +59,7 @@ public class PixelCanvas extends View {
     ScaleGestureDetector sgd;
     GestureDetector gestureDetector;
     GestureDetector shapeMove;
-
+    OnTouchListener onTouchListener;
     public PixelCanvas(Context context, AttributeSet attrs) {
         super(context, attrs);
         //Init touch detectors
@@ -86,7 +85,7 @@ public class PixelCanvas extends View {
             public boolean onSingleTapConfirmed(MotionEvent e) {
                 mode = preMode;
                 movable = false;
-                drawShape();
+                GraphicUlti.drawShape((int)pvTop,(int)pvLeft,preview,bitmap);
                 invalidate();
                 return true;
             }
@@ -102,6 +101,59 @@ public class PixelCanvas extends View {
                 return true;
             }
         });
+        onTouchListener = new OnTouchListener() {
+            private int shapeStartX;
+            private int shapeStartY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        shapeStartX = roundedX;
+                        shapeStartY = roundedY;
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        int preViewWidth = Math.abs(shapeStartX - roundedX) + 1;
+                        int preViewHeight = Math.abs(shapeStartY - roundedY) + 1;
+                        preview = Bitmap.createBitmap(preViewWidth, preViewHeight, Bitmap.Config.ARGB_8888);
+                        pvLeft = Math.min(shapeStartX, roundedX);
+                        pvTop = Math.min(shapeStartY, roundedY);
+                        switch (mode) {
+                            case CUT:
+                                GraphicUlti.drawCut((int)pvTop,(int)pvLeft,bitmap,preview);
+                                break;
+                            case LINE:
+                                int x1=shapeStartX - (int)pvLeft;
+                                int y1=shapeStartY - (int)pvTop;
+                                int x2=roundedX - (int)pvLeft;
+                                int y2=roundedY - (int)pvTop;
+                                GraphicUlti.drawLine(x1,y1,x2,y2,preview,brushColor,brushSize);
+                                break;
+                            case RECT:
+                                GraphicUlti.drawRect(preview,brushColor,brushSize);
+                                break;
+                            case CIRCLE:
+                                GraphicUlti.drawCircle();
+                                break;
+                        }
+                        movable = true;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if(movable) {
+                            if(mode == DRAWMODE.CUT){
+                                for(int x=0;x<preview.getWidth();x++){
+                                    for(int y=0;y<preview.getHeight();y++){
+                                        bitmap.setPixel((int)pvLeft+x,(int)pvTop+y,0);
+                                    }
+                                }
+                            }
+                            preMode = mode;
+                            mode = DRAWMODE.MOVE;
+                        }
+                        break;
+                }
+                return false;
+            }
+        };
         //Init paint for canvas
         paint = new Paint();
         paint.setAntiAlias(false);
@@ -184,6 +236,17 @@ public class PixelCanvas extends View {
         }
     }
 
+    public void setMode(DRAWMODE mode) {
+        switch (mode){
+            case PICK:
+            case MOVE:
+                preMode = mode;
+            default:
+                this.mode = mode;
+
+        }
+    }
+
     public void setNull(){
         for(int i=historyCounter; i<5; i++){
             bitmapHistory[i] = null;
@@ -198,36 +261,31 @@ public class PixelCanvas extends View {
         if (pointerCount > 1) {
             dragAndScale(event);
         } else {
-            event.getPointerId(0);
-            switch (event.getActionMasked()) {
-                case MotionEvent.ACTION_DOWN:
-                    updateBitmapHistory();
-                default:
-                    break;
-            }
-
-            float x = event.getX();
-            float y = event.getY();
-            roundedX = (int) Math.floor(x / scale - left);
-            roundedY = (int) Math.floor(y / scale - top);
+            roundedX = (int) Math.floor(event.getX() / scale - left);
+            roundedY = (int) Math.floor(event.getY() / scale - top);
             if(mode == DRAWMODE.MOVE){
                 shapeMove.onTouchEvent(event);
             }else {
-                if(event.getActionMasked()==MotionEvent.ACTION_DOWN)updateBitmapHistory();
+                if(event.getActionMasked()==MotionEvent.ACTION_DOWN) {
+                    updateBitmapHistory();
+                }
                 switch (mode) {
                     case PEN:
                         System.out.println(scale);
                         System.out.println(height + " " + width);
-                        //System.out.println(touchStartX + " " + touchStartY);
-                        drawPixel(roundedX, roundedY,brushColor);
+                        GraphicUlti.drawPoint(roundedX,roundedY,brushColor,bitmap,brushSize);
                         break;
                     case FILL:
                         floodFill(roundedX, roundedY, bitmap.getPixel(roundedX, roundedY));
                         break;
+                    case PICK:
+                        pickColor();
+                        setMode(preMode);
                     case CUT:
                     case LINE:
                     case RECT:
-                        makeShape(event);
+                    case CIRCLE:
+                        onTouchListener.onTouch(this,event);
                         break;
                 }
             }
@@ -249,126 +307,8 @@ public class PixelCanvas extends View {
         top = Math.min(0, Math.max(top, height / scale - imgW));
     }
 
-    private void makeShape(MotionEvent event) {
-        switch (event.getActionMasked()) {
-            case MotionEvent.ACTION_DOWN:
-                shapeStartX = roundedX;
-                shapeStartY = roundedY;
-                break;
-            case MotionEvent.ACTION_MOVE:
-                int preViewWidth = Math.abs(shapeStartX - roundedX) + 1;
-                int preViewHeight = Math.abs(shapeStartY - roundedY) + 1;
-                preview = Bitmap.createBitmap(preViewWidth, preViewHeight, Bitmap.Config.ARGB_8888);
-                pvLeft = Math.min(shapeStartX, roundedX);
-                pvTop = Math.min(shapeStartY, roundedY);
-                switch (mode) {
-                    case CUT:
-                        drawCut();
-                        break;
-                    case LINE:
-                        drawLine();
-                        break;
-                    case RECT:
-                        drawRect();
-                        break;
-                    case CIRCLE:
-                        drawCircle();
-                        break;
-                }
-                movable = true;
-                break;
-            case MotionEvent.ACTION_UP:
-                if(movable) {
-                    if(mode == DRAWMODE.CUT){
-                        for(int x=0;x<preview.getWidth();x++){
-                            for(int y=0;y<preview.getHeight();y++){
-                                bitmap.setPixel((int)pvLeft+x,(int)pvTop+y,0);
-                            }
-                        }
-                    }
-                    preMode = mode;
-                    mode = DRAWMODE.MOVE;
-                }
-                break;
-        }
-    }
-
-    private void drawShape(){
-        for(int y=0;y<preview.getHeight();y++){
-            for(int x=0;x<preview.getWidth();x++){
-                int pixel = preview.getPixel(x,y);
-                if(pixel!=0){
-                    int bX = (int)(x+pvLeft);
-                    int bY = (int)(y+pvTop);
-                    drawPixel(bX,bY,pixel);
-                }
-            }
-        }
-    }
-
-    private void drawCut(){
-        for(int x=0;x<preview.getWidth();x++){
-            for(int y=0;y<preview.getHeight();y++){
-                preview.setPixel(x,y,bitmap.getPixel(x+(int)pvLeft,y+(int)pvTop));
-            }
-        }
-    }
-
-    private void drawRect(){
-        for(int x=0;x<preview.getWidth();x++){
-            preview.setPixel(x,0,brushColor);
-            preview.setPixel(x,preview.getHeight()-1,brushColor);
-        }
-        for(int y=0;y<preview.getHeight();y++){
-            preview.setPixel(0,y,brushColor);
-            preview.setPixel(preview.getWidth()-1,y,brushColor);
-        }
-    }
-
-    @SuppressWarnings("SuspiciousNameCombination")
-    private void drawLine() {
-        int x1=shapeStartX - (int)pvLeft;
-        int y1=shapeStartY - (int)pvTop;
-        int x2=roundedX - (int)pvLeft;
-        int y2=roundedY - (int)pvTop;
-        boolean steep = Math.abs(y1 - y2) > Math.abs(x1 - x2);
-        if (steep) {
-            int temp;
-            temp = x1;
-            x1 = y1;
-            y1 = temp;
-            temp = x2;
-            x2 = y2;
-            y2 = temp;
-        }
-        if (x1 > x2) {
-            int temp = x1;
-            x1 = x2;
-            x2 = temp;
-            temp = y1;
-            y1 = y2;
-            y2 = temp;
-        }
-        float dx = x2 - x1;
-        float dy = Math.abs(y2 - y1);
-
-        float error = 0;
-        int yStep = (y1 < y2) ? 1 : -1;
-        int y = y1;
-
-        int maxX = x2;
-        for (int x = x1; x <= maxX; x++) {
-            if (steep) {
-                preview.setPixel(y, x, brushColor);
-            } else {
-                preview.setPixel(x, y, brushColor);
-            }
-            error += (dy/dx);
-            if (error > 0.5f) {
-                y += yStep;
-                error -= 1;
-            }
-        }
+    private void pickColor(){
+        brushColor = bitmap.getPixel(roundedX,roundedY);
     }
 
     private void floodFill(int x, int y, int floodColor) {
@@ -381,13 +321,5 @@ public class PixelCanvas extends View {
         floodFill(x, y + 1, color);
         floodFill(x - 1, y, color);
         floodFill(x, y - 1, color);
-    }
-
-    private void drawCircle(){
-
-    }
-    private void drawPixel(int x, int y,int color){
-        if (x >= 0 && x < imgW && y >= 0 && y < imgH)
-            bitmap.setPixel(x, y, brushColor);
     }
 }
